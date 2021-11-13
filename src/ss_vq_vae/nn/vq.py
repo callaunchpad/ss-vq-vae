1d8457f8bfe933076a12c7e8636ac187692ccc72
Hannah <hphuang@berkeley.edu>
2021-11-13 13:31:45 -0800

    trained vq-vae with mus and sigmas (not log sigmas), no kl divergence or doubling of channels yet

--- src/ss_vq_vae/nn/vq.py
+++ src/ss_vq_vae/nn/vq.py
@@ -5,6 +5,8 @@
 import confugue
 import torch
 from torch import nn
+from torch.distributions.normal import Normal
+from torch.distributions import kl_divergence
 
 
 @confugue.configurable
@@ -12,7 +14,7 @@ class VQEmbedding(nn.Module):
 
     def __init__(self, num_embeddings, embedding_dim, use_codebook_loss=True, axis=-1):
         super().__init__()
-        self.embedding = nn.Embedding(num_embeddings, embedding_dim)
+        self.embedding = nn.Embedding(num_embeddings, embedding_dim) # dim * 2, to have mu and logvar 
         self._use_codebook_loss = use_codebook_loss
         self._cfg['init'].bind(nn.init.kaiming_uniform_)(self.embedding.weight)
         self._axis = axis
@@ -20,13 +22,23 @@ class VQEmbedding(nn.Module):
     def forward(self, input):
         if self._axis != -1:
             input = input.transpose(self._axis, -1)
+        print("Input shape: ", input.shape)
+        # mu, logvar = input.chunk(2, dim=1) # check dims 
 
+        # q_z_x = Normal(mu, logvar.mul(.5).exp())
+        # p_z = Normal(torch.zeros_like(mu), torch.ones_like(logvar))
+
+        # x_tilde = self.decoder(q_z_x.rsample())
+        ##
+
+        # distances = kl_divergence(q_z_x, p_z).sum(1).mean() # sus 
         distances = (torch.sum(input ** 2, axis=-1, keepdim=True)
                      - 2 * torch.matmul(input, self.embedding.weight.T)
                      + torch.sum(self.embedding.weight ** 2, axis=-1))
         ids = torch.argmin(distances, axis=-1)
         quantized = self.embedding(ids)
-
+        print("Codebook shape: ", quantized.shape)
+        
         losses = {
             'commitment': ((quantized.detach() - input) ** 2).mean(axis=-1)
         }
@@ -44,4 +56,21 @@ class VQEmbedding(nn.Module):
         if self._axis != -1:
             quantized = quantized.transpose(self._axis, -1).contiguous()
 
+        ones = torch.ones([quantized.shape[0], quantized.shape[1], quantized.shape[2]//2]).to(device=quantized.device)
+        z = torch.randn(quantized.shape[0], quantized.shape[1], quantized.shape[2]//2).to(device=quantized.device)
+
+        sample = torch.dstack([ones, z])
+
+        quantized = quantized*sample  
+        quantized = quantized[:,:,quantized.shape[2]//2] + quantized[quantized.shape[2]//2, :, :]
         return quantized, ids, losses
+
+    def reparameterize(self, mu, log_var):
+        """
+        :param mu: mean from the encoder's latent space
+        :param log_var: log variance from the encoder's latent space
+        """
+        std = torch.exp(0.5 * log_var)  # standard deviation
+        eps = torch.randn_like(std)  # randn_like as we need the same size
+        sample = mu + (eps * std)  # sampling as if coming from the input space
+        return sample
