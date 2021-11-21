9a1d1cd33c6c55d9da67d61200e062e24488865e
Hannah <hphuang@berkeley.edu>
2021-11-21 15:44:30 -0800

    added kl divergence, problem with decoder dims

--- src/ss_vq_vae/nn/vq.py
+++ src/ss_vq_vae/nn/vq.py
@@ -14,7 +14,8 @@ class VQEmbedding(nn.Module):
 
     def __init__(self, num_embeddings, embedding_dim, use_codebook_loss=True, axis=-1):
         super().__init__()
-        self.embedding = nn.Embedding(num_embeddings, embedding_dim) # dim * 2, to have mu and logvar 
+        self.embedding = nn.Embedding(num_embeddings, embedding_dim)
+        self.num_embeddings = num_embeddings
         self._use_codebook_loss = use_codebook_loss
         self._cfg['init'].bind(nn.init.kaiming_uniform_)(self.embedding.weight)
         self._axis = axis
@@ -22,19 +23,27 @@ class VQEmbedding(nn.Module):
     def forward(self, input):
         if self._axis != -1:
             input = input.transpose(self._axis, -1)
-        print("Input shape: ", input.shape)
-        # mu, logvar = input.chunk(2, dim=1) # check dims 
+        
+        mu_i = input[:,:,:input.shape[2]//2]
+        sigma_i = torch.exp(0.5 * input[:, :, input.shape[2]//2:])
 
-        # q_z_x = Normal(mu, logvar.mul(.5).exp())
-        # p_z = Normal(torch.zeros_like(mu), torch.ones_like(logvar))
+        mu_e = self.embedding.weight[:,:self.embedding.weight.shape[1]//2]
+        sigma_e = torch.exp(0.5 * self.embedding.weight[:, self.embedding.weight.shape[1]//2:])
 
-        # x_tilde = self.decoder(q_z_x.rsample())
-        ##
+        numerator = matrix_mse(mu_i, mu_e) + torch.sum(sigma_i**2, axis=-1).unsqueeze(-1)
+        denominator = torch.sum(2*sigma_e**2, axis=-1).unsqueeze(0).unsqueeze(0)
+        second_term = numerator/denominator
 
-        # distances = kl_divergence(q_z_x, p_z).sum(1).mean() # sus 
-        distances = (torch.sum(input ** 2, axis=-1, keepdim=True)
-                     - 2 * torch.matmul(input, self.embedding.weight.T)
-                     + torch.sum(self.embedding.weight ** 2, axis=-1))
+        sigma_e = sigma_e.unsqueeze(0).unsqueeze(0)
+        sigma_i = sigma_i.unsqueeze(axis=-2).repeat(1, 1, self.num_embeddings, 1)
+        log_term = torch.log(sigma_e/sigma_i).sum(axis=-1)
+
+        distances = log_term + second_term
+        
+
+        # distances = (torch.sum(input ** 2, axis=-1, keepdim=True)
+        #              - 2 * torch.matmul(input, self.embedding.weight.T)
+        #              + torch.sum(self.embedding.weight ** 2, axis=-1))
         ids = torch.argmin(distances, axis=-1)
         quantized = self.embedding(ids)
         print("Codebook shape: ", quantized.shape)
@@ -59,11 +68,12 @@ class VQEmbedding(nn.Module):
         ones = torch.ones([quantized.shape[0], quantized.shape[1], quantized.shape[2]//2]).to(device=quantized.device)
         z = torch.randn(quantized.shape[0], quantized.shape[1], quantized.shape[2]//2).to(device=quantized.device)
 
-        sample = torch.dstack([ones, z])
-
-        quantized = quantized*sample  
-        quantized = quantized[:,:,quantized.shape[2]//2] + quantized[quantized.shape[2]//2, :, :]
+        sigmaZ = torch.exp(0.5 * quantized[:, :, quantized.shape[2]//2:]) * z
+        quantized = quantized[:,:,:quantized.shape[2]//2] + sigmaZ
         return quantized, ids, losses
+
+    def matrix_mse(m1, m2):
+        return torch.sum(m1 ** 2, axis=-1, keepdim=True) - 2 * torch.matmul(m1, m2.T) + torch.sum(m2 ** 2, axis=-1)
 
     def reparameterize(self, mu, log_var):
         """
